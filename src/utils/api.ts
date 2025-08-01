5cbe98f6e14a95765875430f6e9f0d77104db1fa
William Winterflood <williamwinterflood@outlook.com>
2025-08-01 17:35:03 +0100

    fixing bugs

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const API_BASE_URL = "https://90906d78e97b.ngrok-free.app";
-
 export async function getAuthHeaders() {
   const idToken = await AsyncStorage.getItem('idToken');
   if (!idToken) {
